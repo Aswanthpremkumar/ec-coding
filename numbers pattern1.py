0847b281e50b58990619649abd0fb104518f2cf8
Aswanthpremkumar <112889192+Aswanthpremkumar@users.noreply.github.com>
2022-09-12 20:33:50 +0530

    Add files via upload

---
+++ numbers pattern1.py
@@ -0,0 +1,11 @@
+
+for k in range(5):
+    for l in range(k):
+        print(str(k),end="*")
+    print("")
+
+for i in range(4,0,-1):
+    for j in range(i):
+        print(str(i),end="*")
+    print("")
+
