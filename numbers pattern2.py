ea5f52fdecfcaa95affe375e7d381376d3e24b9f
Aswanthpremkumar <112889192+Aswanthpremkumar@users.noreply.github.com>
2022-09-14 19:47:24 +0530

    Add files via upload

---
+++ numbers pattern2.py
@@ -0,0 +1,12 @@
+for k in range(5):
+    for l in range(k):
+        a=k+2
+        print(a,end="")
+    print("")
+
+for i in range(3,0,-1):
+    for j in range(i):
+        b=i+2
+        print(b,end="")
+    print("")
+
