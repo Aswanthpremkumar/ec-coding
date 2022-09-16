22a263f8d8c059933a6404667b44a109415d45ca
Aswanthpremkumar <112889192+Aswanthpremkumar@users.noreply.github.com>
2022-09-16 19:31:06 +0530

    Add files via upload

---
+++ even odd.py
@@ -0,0 +1,10 @@
+while True:
+    a=int(input("enter a number:"))
+    if a%2==0:
+        print("Even")
+    else:
+        print("Odd")
+    if a%1==0 & a%a==0:
+        print("prime number")
+    else:
+        print("not a prime number")
