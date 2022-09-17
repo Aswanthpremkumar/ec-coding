f2e1e8f938799cae5f548257cbd6f3a357dcf172
Aswanthpremkumar <112889192+Aswanthpremkumar@users.noreply.github.com>
2022-09-17 19:51:49 +0530

    Add files via upload

---
+++ number's detail.py
@@ -0,0 +1,15 @@
+while True:
+    num=int(input("enter a number:"))
+    if num%2==0:
+        print("Even")
+    else:
+        print("Odd")
+    
+    if num > 1:
+   
+       for i in range(2,num):
+           if (num % i) == 0:
+               print(num,"is not a prime number")
+               break
+       else:
+           print(num,"is a prime number")
